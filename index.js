8b990e689a1ab9a07c7bff3c729396064a9169d7
Sharifdesigns22 <shariftarver@gmail.com>
2021-07-12 03:08:00 -0400

    commit tree bf0f5b0d8fc55538b96e4c1a6e11ea416eb55f74

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateText();
+  });
+
+  updateText = () => {
+      document.getElementById("text").textContent = "This is really cool!";
+  };
